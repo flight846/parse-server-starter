ee5b2f022c22af7b319c5c62d4643a121ed2bd99
flight846 <flight846@gmail.com>
2017-05-26 16:52:58 +0800

    Add mailgun code

--- index.js
+++ index.js
@@ -9,16 +9,36 @@ var bodyParser = require('body-parser');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+
+    //**** Email Verification ****//
+    /* Enable email verification */
+    verifyUserEmails: true,
+    /* The public URL of your app * /
+    // This will appear in the link that is used to verify email addresses and reset passwords.
+    /* Set the mount path as it is in serverURL */
+    publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+    /* This will appear in the subject and body of the emails that are sent */
+    appName: process.env.APP_NAME || "Resume Builder 1.0",
+
+    emailAdapter: {
+    	module: 'parse-server-simple-mailgun-adapter',
+    	options: {
+    		fromAddress: process.env.EMAIL_FROM || "test@example.com",
+    		domain: process.env.MAILGUN_DOMAIN || "example.com",
+    		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
+    	}
+    }
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -41,7 +61,9 @@ app.use(express.static('public'));
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
